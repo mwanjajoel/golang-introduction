ffae5bce32358c4b45ed327a721e9379d879e956
mwanjajoel <joelsilverworks@gmail.com>
2016-02-07 14:03:05 +0300

    Began on the simple Go Program

---
+++ main.go
@@ -0,0 +1,31 @@
+package Go
+
+//including Packages we shall be using in this simple program
+import (
+
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Wassup my people \n")
+
+	//calling methods that are defined at the bottom
+	dealingWithNumbers()
+	getStringLength()
+}
+
+//dealing with numbers, in a separate method
+func dealingWithNumbers() {
+
+	fmt.Println("1 + 1 =", 1 + 1)
+
+}
+
+//lets get the length of a string, real quick
+func getStringLength() {
+
+	fmt.Println(len("Wassup my people"))
+
+}
+
